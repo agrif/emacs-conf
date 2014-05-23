e599f5af2820b3156d018e0bb8dd09b9bea6e883
Aaron Griffith <aargri@gmail.com>
2014-05-23 11:23:26 -0400

    4 space indent, no tabs

--- dot-emacs.el
+++ dot-emacs.el
@@ -3,12 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(c-default-style "bsd")
  '(geiser-mode-smart-tab-p t)
+ '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initial-major-mode (quote emacs-lisp-mode))
  '(make-backup-files nil)
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
+ '(tab-width 4)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
