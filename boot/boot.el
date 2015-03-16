210266ca24ffdf3704d93606ff55c81f8cbf989d
Aaron Griffith <aargri@gmail.com>
2015-03-16 15:59:40 -0400

    do not warn about .emacs.d in load path (living dangerously!)

--- boot/boot.el
+++ boot/boot.el
@@ -10,6 +10,15 @@
 (load "boot/python")
 (load "boot/lisp")
 
+;; squelch the warning about .emacs.d in load path
+(defadvice display-warning
+    (around no-warn-.emacs.d-in-load-path (type message &rest unused) activate)
+  "Ignore the warning about `.emacs.d' in `load-path'."
+  (unless (and (eq type 'initialization)
+               (string-prefix-p "Your `load-path' seems to contain\nyour `.emacs.d' directory"
+                                message t))
+    ad-do-it))
+
 ;; start the emacs server
 (require 'server)
 (if (not (server-running-p))
