ff00014da9bf034e04d56bf2d2af5895a07871a5
Aaron Griffith <aargri@gmail.com>
2016-02-03 14:55:14 -0500

    flymake, compilation, cargo for rust

--- boot/rust.el
+++ boot/rust.el
@@ -2,11 +2,19 @@
 (require-package 'racer)
 (require-package 'flycheck-rust)
 (require-package 'rustfmt)
+(require-package 'cargo)
 
 (eval-after-load "rust-mode-autoloads"
   '(progn
      (setq racer-cmd "/home/agrif/.multirust/toolchains/nightly/cargo/bin/racer")
      (setq racer-rust-src-path "/home/agrif/local/rust/src/")
      (add-hook 'rust-mode-hook #'racer-mode)
+     (add-hook 'rust-mode-hook #'cargo-minor-mode)
      (add-hook 'racer-mode-hook #'eldoc-mode)
-     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+     (add-hook 'rust-mode-hook #'flycheck-mode)
+     (add-hook 'rust-mode-hook #'flycheck-rust-setup)
+
+     ;; use cargo to compile
+     (add-hook 'rust-mode-hook
+               (lambda ()
+                 (set (make-local-variable 'compile-command) "cargo build")))))
