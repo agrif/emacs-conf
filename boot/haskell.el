2bc8fabf57370442d1badcd925670114dd7e2baf
Aaron Griffith <aargri@gmail.com>
2016-05-04 19:13:13 -0400

    ghc-mod

--- boot/haskell.el
+++ boot/haskell.el
@@ -1,4 +1,8 @@
 ;; haskell with interaction
 (require-package 'haskell-mode)
+(require-package 'ghc)
 (eval-after-load "haskell-mode-autoloads"
-  '(add-hook 'haskell-mode-hook 'interactive-haskell-mode))
+  '(progn
+     (require 'ghc)
+     (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+     (add-hook 'haskell-mode-hook (lambda () (ghc-init)))))
