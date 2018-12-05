669f4829f14dcd34c3dd85cfc03443cc7107d6b9
Aaron Griffith <aargri@gmail.com>
2018-12-05 13:57:27 -0500

    updated theme.el to use-package

--- boot/theme.el
+++ boot/theme.el
@@ -1,7 +1,6 @@
 ;; moe!
-(require-package 'moe-theme)
-(eval-after-load "moe-theme-autoloads"
-  '(progn
-     (require 'moe-theme)
-     (moe-theme-set-color 'orange)
-     (load-theme 'moe-dark t)))
+(use-package moe-theme
+  :demand
+  :config
+  (load-theme 'moe-dark t)
+  (moe-theme-set-color 'orange))
