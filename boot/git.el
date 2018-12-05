36e78c8a60bbceab1b94ad55a9180103b6f57753
Aaron Griffith <aargri@gmail.com>
2018-12-05 15:09:12 -0500

    converted and updated git.el to use-package

--- boot/git.el
+++ boot/git.el
@@ -1,18 +1,14 @@
-(require-package 'gist)
-(require-package 'git-link)
-(require-package 'magit)
+(use-package gist)
+(use-package magit
+  :bind ("C-x g" . magit-status))
 
-(eval-after-load "git-link-autoloads"
-  '(progn
-     (defun browse-git-link ()
-       "Browse to the url provided by (git-link)."
-       (interactive)
-       (call-interactively 'git-link)
-       (browse-url (car kill-ring-yank-pointer)))))
-(eval-after-load "git-link" ; special handling for gh: special remotes
-  '(progn
-     (add-to-list 'git-link-remote-alist '("gh" git-link-github))))
-
-(eval-after-load "magit-autoloads"
-  '(progn
-     (global-set-key (kbd "C-x g") 'magit-status)))
+;; a little bit of special stuff for git-link
+(defun git-link-gh-github (HOSTNAME DIRNAME FILENAME BRANCH COMMIT START END)
+  (git-link-github "github.com" DIRNAME FILENAME BRANCH COMMIT START END))
+(defun git-link-gh-github-commit (HOSTNAME DIRNAME COMMIT)
+  (git-link-github "github.com" DIRNAME COMMIT))
+(use-package git-link
+  :custom (git-link-open-in-browser t)
+  :config
+  (add-to-list 'git-link-remote-alist '("gh" git-link-gh-github))
+  (add-to-list 'git-link-commit-remote-alist '("gh" git-link-gh-github-commit)))
