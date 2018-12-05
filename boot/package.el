accc27e887aaec7e199fdf308cef013b3c562932
Aaron Griffith <aargri@gmail.com>
2018-12-05 13:00:34 -0500

    load use-package

--- boot/package.el
+++ boot/package.el
@@ -1,11 +1,32 @@
-;; setup for package.el
+;; use the package manager
+(require 'package)
 
 ;; repo urls
 (setq package-archives 
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
+
+;; initialize here, since we don't in init.el
 (package-initialize)
+
+;; we sort of depend on use-package always being here, so, install it.
+;; this is the only package we explicitly install during boot
+(unless (package-installed-p 'use-package)
+    (unless package-archive-contents
+      (package-refresh-contents))
+    (package-install 'use-package))
+
+;; ok, now load it in the recommended way
+(eval-when-compile
+  (require 'use-package))
+
+;; if we mention it, default to trying to install it
+(setq use-package-always-ensure t)
+
+;;
+;; Everything after this point is deprecated! Use use-package instead.
+;;
 
 ;; quick helper to auto-fetch-install our packages
 (setq packages '())
