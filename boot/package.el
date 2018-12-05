0546d632c39f80ada235cce0f36f98c2e356b60c
Aaron Griffith <aargri@gmail.com>
2018-12-05 16:58:08 -0500

    remove old package code

--- boot/package.el
+++ boot/package.el
@@ -25,23 +25,3 @@
 (setq use-package-always-ensure t)
 ;; if we mention it, default to loading deferred
 (setq use-package-always-defer t)
-
-;;
-;; Everything after this point is deprecated! Use use-package instead.
-;;
-
-;; quick helper to auto-fetch-install our packages
-(setq packages '())
-
-(defun require-package (p)
-  (add-to-list 'packages p))
-
-(defun package-install-all ()
-  "Auto fetch and install agrif-approved packages."
-  (interactive)
-  (package-refresh-contents)
-  (mapc (lambda (s) 
-	  (if (not (package-installed-p s))
-	      (package-install s)))
-	packages)
-  (message "Done."))
