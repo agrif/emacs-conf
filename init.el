739c70d4ca57ead1b8efb3ea3530cc02374e2525
Aaron Griffith <aargri@gmail.com>
2018-12-05 12:28:36 -0500

    moved boot/boot.el contents to init.el

--- init.el
+++ init.el
@@ -1,9 +1,33 @@
-;; quiet down package manager (this is done later)
+;; We do our own package initialization.
+;; If this is not here, emacs will re-add it, uncommented. Yes, really.
 ;;(package-initialize)
 
 ;; store customized settings in their own file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;; call out to the local setup
-(load "~/.emacs.d/boot/boot.el")
+;; load our package management and general utility code first
+(load "~/.emacs.d/boot/package.el")
+(load "~/.emacs.d/boot/util.el")
+
+;; now load everything else
+(load "~/.emacs.d/boot/majormodes.el")
+(load "~/.emacs.d/boot/editor.el")
+(load "~/.emacs.d/boot/compile.el")
+(load "~/.emacs.d/boot/theme.el")
+(load "~/.emacs.d/boot/applications.el")
+(load "~/.emacs.d/boot/git.el")
+(load "~/.emacs.d/boot/python.el")
+(load "~/.emacs.d/boot/lisp.el")
+(load "~/.emacs.d/boot/rust.el")
+(load "~/.emacs.d/boot/haskell.el")
+
+;; otherwise, .v would mean coq
+(setq auto-mode-alist
+      (remove (rassoc 'coq-mode auto-mode-alist) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.v\\'" . verilog-mode))
+
+;; start the emacs server
+(require 'server)
+(if (not (server-running-p))
+    (server-start))
