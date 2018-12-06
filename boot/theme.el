9a5a0c18f70694e15ba13d898c616663fe3abc7b
Aaron Griffith <aargri@gmail.com>
2018-12-06 17:08:32 -0500

    use powerline in UI

--- boot/theme.el
+++ boot/theme.el
@@ -4,3 +4,10 @@
   :config
   (load-theme 'moe-dark t)
   (moe-theme-set-color 'orange))
+
+;; and powerline
+(use-package powerline
+  :demand
+  :config
+  (when (display-graphic-p)
+    (powerline-default-theme)))
