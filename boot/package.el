0b1ec6a9905ec8918b6dbc989c630faf3387d182
Aaron Griffith <aargri@gmail.com>
2019-03-02 00:01:13 -0500

    use just melpa, not marmalade, to avoid version clashes

--- boot/package.el
+++ boot/package.el
@@ -4,7 +4,6 @@
 ;; repo urls
 (setq package-archives 
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
 
 ;; initialize here, since we don't in init.el
