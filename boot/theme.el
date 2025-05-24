5aeb89002e19d8c036f9ddac4ab96e0889d93aed
Aaron Griffith <aargri@gmail.com>
2025-05-24 06:17:30 -0400

    fix for tiny frame bug

--- boot/theme.el
+++ boot/theme.el
@@ -1,5 +1,16 @@
 (require 'ensure-use-package "~/.emacs.d/boot/ensure-use-package")
 
+;; https://emacs.stackexchange.com/a/83204
+;; HACK Resize new frames shortly after creation.  Works around
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=67654
+(setq initial-frame-alist '((width . 80) (height . 36)))
+(add-hook 'after-make-frame-functions
+   (defun +resize-after-make-frame (frame)
+     (let ((width (/ (frame-text-width frame) (frame-char-width frame)))
+           (height (/ (frame-text-height frame) (frame-char-height frame))))
+       (sleep-for 0 100)
+       (set-frame-size frame width height))))
+
 ;; moe!
 (use-package moe-theme
   :demand
